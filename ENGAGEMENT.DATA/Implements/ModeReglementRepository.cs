04953e57a1e55b9404c6f3e4e949ba76ca3ede28
Aida ATHAMNIA <aida.athamnia@novencia.com>
2020-06-09 11:09:07 +0100

    Ajout services et repository pour ModeReglement , banque et caisse

---
+++ ENGAGEMENT.DATA/Implements/ModeReglementRepository.cs
@@ -0,0 +1,13 @@
+﻿using ENGAGEMENT.ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGAGEMENT.DATA.Implements
+{
+   public class ModeReglementRepository
+    {
+    }
+}
